12a6dcf6c3f6827b9dcabea53e02263123180a0a
Anurag Prasoon <anuragprasoon23@gmail.com>
2022-07-05 00:24:58 +0530

    Time: 217 ms (46.66%), Space: 16.6 MB (84.31%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,5 +1,6 @@
 class Solution:
+    from collections import Counter
     def singleNumber(self, nums: List[int]) -> int:
-       for i in nums:
-        if nums.count(i)==1:
-            return i
+        dict = Counter(nums)
+        return [num for num,freq in dict.items() if freq == 1][0]
+        
