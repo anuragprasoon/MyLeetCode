d724c899a6fa69d7dd37154b99d0222fe1e30b86
Anurag Prasoon <anuragprasoon23@gmail.com>
2022-07-04 23:54:04 +0530

    Time: 67 ms (17.70%), Space: 13.9 MB (8.74%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
@@ -1,7 +1,4 @@
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
         nums=[nums[i] for i in range(0,len(nums)) if nums.count(nums[i])==1 ]
-        s=0
-        for i  in nums:
-            s+=i
-        return s
+        return sum(nums)
